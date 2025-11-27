dc6231b38d5dd03a31c4ee969c2cde8fedfc4b74
PhoenixHydra <lizencore@pm.me>
2025-11-27 11:05:56 +0100

    Add files via upload

---
+++ src/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+    nebulaStart: '#6a11cb',
+    nebulaEnd: '#2575fc',
+    bgColor: '#f4f6f8',
+    cardBg: '#ffffff',
+    textPrimary: '#1c1c1e',
+    textSecondary: '#636366',
+    borderColor: '#e5e5e5',
+    scoreA: '#2ecc71',
+    scoreB: '#a5d6a7',
+    scoreC: '#f39c12',
+    scoreD: '#e74c3c',
+    scoreE: '#b71c1c',
+};
